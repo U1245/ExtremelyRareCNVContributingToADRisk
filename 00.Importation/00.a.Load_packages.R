91a943398b1c7b6975f8d570c48d498a6d4e3638
CatherineSchramm <41482329+CatherineSchramm@users.noreply.github.com>
2024-11-22 10:24:03 +0100

    Add files via upload

---
+++ 00.Importation/00.a.Load_packages.R
@@ -0,0 +1,17 @@
+############################################# #
+## R libraries #### 
+############################################# #
+
+
+# R 3.6.0
+library(foreach)
+library(ordinal)
+library(parallel)
+library(bigmemory)
+library(MASS)
+library(ggplot2)
+library(tidyverse)
+library(ggrepel)
+library(gridExtra)
+library(brglm2)
+library(logistf)
